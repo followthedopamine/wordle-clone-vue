0d74eccd252a19053b06f3f00ee8f22bbec53c85
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-10-08 01:03:19 +1300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import GridBoxVue from "./components/GridBox.vue";
+const gridSize = [5, 6];
+
+const createGrid = ([rows, cols]) => {
+  return new Array(rows).fill(new Array(cols).fill("~"));
+};
+
+const grid = createGrid(gridSize);
+console.log(grid);
+</script>
+
+<template>
+  <div class="row" v-for="(row, index) in grid" :key="index">
+    <span class="col" v-for="(col, index) in row" :key="index">{{ col }}</span>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #111;
+  color: white;
+}
+</style>
---
+++ src/components/GridBox.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { defineProps } from "vue";
+const props = defineProps({
+  value: String,
+});
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style></style>
