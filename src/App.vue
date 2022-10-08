78968a6178e8801d7f18b7a4ab2dbb91ce92cff0
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-10-08 15:11:31 +1300

    Added keyboard input to grid

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,84 @@
 <script setup>
-import GridBoxVue from "./components/GridBox.vue";
-const gridSize = [5, 6];
+import GridBox from "./components/GridBox.vue";
+import { ref } from "vue";
 
 const createGrid = ([rows, cols]) => {
-  return new Array(rows).fill(new Array(cols).fill("~"));
+  const g = new Array(rows);
+  for (let i = 0; i < g.length; i++) {
+    g[i] = new Array(cols).fill(" ");
+  }
+  return g;
 };
 
+const inputCharToGrid = (char) => {
+  if (currentCol < wordLength) {
+    grid[currentRow][currentCol] = char;
+    currentCol++;
+  }
+};
+
+const handleKeyboard = (event) => {
+  console.log(event.key);
+  inputCharToGrid(event.key);
+  forceRerender();
+};
+
+const componentKey = ref(0);
+
+const forceRerender = () => {
+  componentKey.value += 1;
+};
+
+const wordLength = 5;
+const numberOfGuesses = 6;
+const gridSize = [numberOfGuesses, wordLength];
 const grid = createGrid(gridSize);
-console.log(grid);
+let currentRow = 0;
+let currentCol = 0;
+document.body.addEventListener("keydown", handleKeyboard);
 </script>
 
 <template>
-  <div class="row" v-for="(row, index) in grid" :key="index">
-    <span class="col" v-for="(col, index) in row" :key="index">{{ col }}</span>
+  <div class="container" :key="componentKey">
+    <div>
+      <div
+        class="row"
+        :class="'row' + index"
+        v-for="(row, index) in grid"
+        :key="'row' + index"
+      >
+        <GridBox
+          v-for="(col, index) in row"
+          :key="'col' + index"
+          :value="col"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <style>
+:root {
+  --green: #6aaa64;
+  --yellow: #c9b458;
+  --dark-grey: #333;
+  --light-grey: #555;
+}
+
 body {
-  background-color: #111;
+  background-color: #222;
   color: white;
+  font-size: 30px;
+  font-family: "Clear Sans", "Helvetica Neue", Arial, sans-serif;
+  font-weight: bold;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.row {
+  display: flex;
 }
 </style>
--- src/components/GridBox.vue
+++ src/components/GridBox.vue
@@ -6,7 +6,17 @@ const props = defineProps({
 </script>
 
 <template>
-  <div></div>
+  <span>{{ props.value }}</span>
 </template>
 
-<style></style>
+<style scoped>
+span {
+  display: flex;
+  width: 50px;
+  height: 50px;
+  margin: 4px;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid var(--dark-grey);
+}
+</style>
